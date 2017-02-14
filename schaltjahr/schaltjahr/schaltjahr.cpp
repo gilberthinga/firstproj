9a66137af7df924d273055aea5d25036078d1a95
ghinga <ghinga@PC04124302>
2017-02-14 11:35:05 +0100

    Fertig2

--- schaltjahr/schaltjahr/schaltjahr.cpp
+++ schaltjahr/schaltjahr/schaltjahr.cpp
@@ -15,7 +15,7 @@ int main()
 	
 	erg = jahr % 4 == 0 && (jahr % 100 != 0 || jahr % 400 == 0);
 
-	erg == 1 ? printf("Das %i ist ein Schaltjahr\n", jahr) : printf("Das %i ist kein Schaltjahr\n", jahr);
+	erg == 1 ? printf("Das Jahr %i ist ein Schaltjahr\n", jahr) : printf("Das Jahr %i ist kein Schaltjahr\n", jahr);
 
 
     return 0;
