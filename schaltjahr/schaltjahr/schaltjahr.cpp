1a2bc641c79ffccf8ac86ec689709fb7451721d1
ghinga <ghinga@PC04124302>
2017-02-14 10:54:43 +0100

    Schaltjahr Fertig

---
+++ schaltjahr/schaltjahr/schaltjahr.cpp
@@ -0,0 +1,23 @@
+// schaltjahr.cpp : Definiert den Einstiegspunkt für die Konsolenanwendung.
+//
+
+#include <stdio.h>
+
+
+
+int main()
+{
+
+	int jahr, erg;
+
+	printf("Bitte geben sie ein Jahr ein:\n");
+	scanf("%i", &jahr);
+	
+	erg = jahr % 4 == 0 && (jahr % 100 != 0 || jahr % 400 == 0);
+
+	erg == 1 ? printf("Das %i ist ein Schaltjahr\n", jahr) : printf("Das %i ist kein Schaltjahr\n", jahr);
+
+
+    return 0;
+}
+
